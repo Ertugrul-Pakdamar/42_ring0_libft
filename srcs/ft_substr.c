98537c4f163a8816c8497e7d4786940505a12014
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-06-02 11:25:49 +0300

    Improves ft_substr robustness
    Improves the ft_substr function by using ft_strlen to calculate the string length, preventing potential issues when dealing with large strings.
    
    Also, adds a check to ensure that the requested substring length does not exceed the available characters after the start index. If it does, adjusts the length to avoid out-of-bounds access.

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 08:46:59 by epakdama          #+#    #+#             */
-/*   Updated: 2025/05/20 10:17:10 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/06/02 11:22:43 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char			*res;
-	unsigned int	index;
-	unsigned int	s_len;
+	char	*res;
+	size_t	index;
+	size_t	s_len;
 
 	if (s == NULL)
 		return (NULL);
-	s_len = 0;
-	while (s[s_len])
-		s_len++;
+	s_len = ft_strlen(s);
+	if (len > s_len - start)
+		len = s_len - start;
 	res = (char *)malloc(len + 1);
 	if (res == NULL)
 		return (NULL);
